1b2ee65359ca89d10c18879f0787ef7d3c3a2c11
Ahmed Soliman <ahmed.soli101@gmail.com>
2021-08-12 00:40:53 +0200

    finish gif react app

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+const apiKey=process.env.REACT_APP_API_KEY;
 const Random= ()=>{
+    const [gif, setGif]=useState('')
+
+    const fetchGif= async()=>{
+        const url=`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+        const {data}= await axios.get(url)
+        const imgUrl= data.data.images.downsized.url
+        setGif(imgUrl)
+    }
+    useEffect(()=>{fetchGif();},[]);
+
+
+    const handleClick= ()=>{
+        fetchGif()
+    }
     return(
-        <h1>Random</h1>
+        <div className="container">
+            <h1>Random</h1>
+            <img width="500" height="500" src={gif} alt="Random Gif"/>
+            <button onClick={handleClick}>Get New Gif</button>
+        </div>
+
     )
 }
-
-
 export default Random;
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+const apiKey=process.env.REACT_APP_API_KEY;
 const Tag= ()=>{
+    const [gif, setGif]=useState();
+    const [tag, setTag]= useState('basketball')
+
+    const fetchGif= async(tag)=>{
+        const url=`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${tag}`;
+        const {data}= await axios.get(url)
+        const imgUrl= data.data.images.downsized.url;
+        setGif(imgUrl)
+    }
+    useEffect(()=>{fetchGif(tag);},[tag]);
+
+
+    const handleClick= ()=>{
+        fetchGif(tag)
+    }
     return(
-        <h1>Tag</h1>
+        <div className="container">
+            <h1>The {tag} Gif</h1>
+            <img width="500" height="500" src={gif} alt="Random Gif"/>
+            <input placeholder="Enter Gif Name" onChange={(e)=>{setTag(e.target.value)}} />
+            <button onClick={handleClick}>Get a Gif</button>
+        </div>
+
     )
 }
-
 
 export default Tag;
